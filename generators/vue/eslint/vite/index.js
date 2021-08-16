de66b716e9e58fc7218202caa57349fb71b1c48d
danielpaz98 <alexdaniel-1998@outlook.com>
2021-08-16 03:34:54 -0500

    Add config for eslint vite (vue)

---
+++ generators/vue/eslint/vite/index.js
@@ -0,0 +1,36 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+	viteConfig() {
+		this.fs.copy(this.templatePath("vite.config.js"), this.destinationPath("vite.config.js"));
+	}
+
+	jsConfig() {
+		this.fs.copy(this.templatePath("jsconfig.json"), this.destinationPath("jsconfig.json"));
+	}
+
+	eslintIgnore() {
+		this.fs.copy(this.templatePath(".eslintignore"), this.destinationPath(".eslintignore"));
+	}
+
+	eslintRc() {
+		this.fs.copy(this.templatePath(".eslintrc"), this.destinationPath(".eslintrc"));
+	}
+
+	installDevDependencies() {
+		this.npmInstall(
+			[
+				"prettier",
+				"eslint",
+				"eslint-config-prettier",
+				"eslint-config-standard",
+				"eslint-plugin-import",
+				"eslint-plugin-node",
+				"eslint-plugin-promise",
+				"eslint-plugin-vue",
+				"vite-plugin-eslint",
+			],
+			{ "save-dev": true }
+		);
+	}
+};
